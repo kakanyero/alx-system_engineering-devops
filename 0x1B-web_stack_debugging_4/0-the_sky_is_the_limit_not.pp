0fa481eec163bceb3f7e4cf9cd337328aaff0ea8
kakanyero <kakanyero142@gmail.com>
2023-08-17 10:11:18 +0300

    number one onlyy

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,13 @@
-#0-the_sky_is_the_limit_not.pp set open file limit higher
-exec { 'set limit to 2000':
-  path    => '/bin',
-  command => "sed -i 's/15/2000/' /etc/default/nginx"
-}
-exec { 'reboot nginx':
-  command => '/usr/sbin/service nginx restart'
+# Increases the amount of traffic an Nginx server can handle.
+
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
